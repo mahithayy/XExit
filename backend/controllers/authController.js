abd9fa0422c44d5f2fb0d80b5372e52f9911723c
mahithayy <hollamahitha@gmail.com>
2025-07-28 11:56:30 +0530

    path

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
-const User = require("../models/user");
+const User = require("../models/User");
 
 const JWT_SECRET = process.env.JWT_SECRET || "mysecretkey";
 exports.register = async (req, res) => {
